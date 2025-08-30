e44a82e197c8052170abe2ec6d3efbf98f350e93
trongkhan <trongkhanghqv@gmail.com>
2025-08-30 15:48:15 +0700

    Initial commit
    Generated by react-native@0.81.1

---
+++ android/app/src/main/java/com/aquariux_movie_project/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.aquariux_movie_project
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "Aquariux_Movie_Project"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
